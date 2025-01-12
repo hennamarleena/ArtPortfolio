178915459c7118d0d11644dee8df7a741a1c797a
henna <henna.rajakisto@gmail.com>
2025-01-12 13:24:48 +0200

    email changed

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
 return (
     <footer>
         <div>
-            <p>sa22889@student.samk.fi</p>
+            <p>henna.rajakisto@gmail.com</p>
             <a href="https://www.linkedin.com/in/henna-rajakisto-b57b99253" target="_blank" rel="noopener noreferrer">
                 <img src="img/linkedin-square-icon.png" alt="LinkedIn"/>
             </a>
