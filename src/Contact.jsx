43f645301f72e77f6dffd65710411dd980be2bb3
henna <henna.rajakisto@gmail.com>
2025-01-13 14:19:37 +0200

    form added

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -5,9 +5,17 @@ export default function Contact() {
     return (
         <Container className="contact-container">
             <Box className="email-box">
-            <h2 className="contact-title">Contact</h2>
+            <h2>Email</h2>
             <p>henna.rajakisto@gmail.com</p>
             </Box>
+            
+            <form name="contact" className="contact-form" netlify>
+                <h2>Send a message</h2>
+                <label htmlFor="name">Name<input type="text" id="name" required /></label>
+                <label htmlFor="email">Email<input type="email" id="email" /></label>
+                <label htmlFor="message">Message <span style={{color: "red"}}>*</span><textarea id="message" required /></label>
+                <button type="submit">Send</button>
+            </form>
         </Container>
     )
 }
