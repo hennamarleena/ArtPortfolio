85e8e52873425bb2e501f111e64c87a706cd39ff
henna <henna.rajakisto@gmail.com>
2025-01-13 20:59:55 +0200

    form deleted

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -8,15 +8,8 @@ export default function Contact() {
             <h2>Email</h2>
             <p>henna.rajakisto@gmail.com</p>
             </Box>
-            
-            <form name="contact" method="POST" className="contact-form" data-netlify="true" action="/thank-you">
-                <input type="hidden" name="contact-form" value="contact" />
-                <h2>Send a message</h2>
-                <label htmlFor="name">Name <span style={{color: "red"}}>*</span> <input type="text" id="name" name="name" required /></label>
-                <label htmlFor="email">Email<input type="email" id="email" name="email" /></label>
-                <label htmlFor="message">Message <span style={{color: "red"}}>*</span><textarea id="message" name="message" required /></label>
-                <button type="submit">Send</button>
-            </form>
+            <Box>
+            </Box>
         </Container>
     )
 }
