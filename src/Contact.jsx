124f0e85c308edafdfacfaa2f13932f293bfc065
henna <henna.rajakisto@gmail.com>
2025-01-14 12:48:17 +0200

    form fixed

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,28 +1,30 @@
 import Container from "@mui/material/Container";
 import Box from '@mui/material/Box';
+import { useState } from "react";
 
 export default function Contact() {
+    const [result, setResult] = useState("");
 
     const onSubmit = async (event) => {
         event.preventDefault();
+        setResult("Sending....");
         const formData = new FormData(event.target);
     
         formData.append("access_key", "6845ebfa-9aae-4dce-889a-3253cffe718e");
     
-        const object = Object.fromEntries(formData);
-        const json = JSON.stringify(object);
-    
-        const res = await fetch("https://api.web3forms.com/submit", {
+        const response = await fetch("https://api.web3forms.com/submit", {
           method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-          },
-          body: json
-        }).then((res) => res.json());
+          body: formData
+        });
     
-        if (res.success) {
-          console.log("Success", res);
+        const data = await response.json();
+    
+        if (data.success) {
+          setResult("Thank you for your message!");
+          event.target.reset();
+        } else {
+          console.log("Error", data);
+          setResult(data.message);
         }
       };
 
@@ -34,11 +36,15 @@ export default function Contact() {
             </Box>
             <form onSubmit={onSubmit} className="contact-form">
                 <h2>Send a message</h2>
-                <label htmlFor="name">Name <span style={{color: "red"}}>*</span><input type="text" id="name" required /></label>
-                <label htmlFor="email">Email <span style={{color: "red"}}>*</span><input type="email" id="email" /></label>
-                <label htmlFor="message">Message <span style={{color: "red"}}>*</span><textarea id="message" required /></label>
+                <label htmlFor="name">Name <span style={{color: "red"}}>*</span><input type="text" id="name" name="name" required /></label>
+                <label htmlFor="email">Email <span style={{color: "red"}}>*</span><input type="email" id="email" name="email" /></label>
+                <label htmlFor="message">Message <span style={{color: "red"}}>*</span><textarea id="message" name="message" required /></label>
+                <input type="hidden" name="access_key" value="6845ebfa-9aae-4dce-889a-3253cffe718e"/>
+                
                 <button type="submit">Send</button>
             </form>
+            <span>{result}</span>
+
         </Container>
     )
 }
