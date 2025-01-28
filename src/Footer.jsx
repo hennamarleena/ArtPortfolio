1eec3698a1673a43fdca725cdbf6408ddf8d240e
henna <henna.rajakisto@gmail.com>
2025-01-28 16:42:33 +0200

    commit 28.1.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,7 @@ return (
         <div>
             <p>henna.rajakisto@gmail.com</p>
             <a href="https://www.linkedin.com/in/henna-rajakisto-b57b99253" target="_blank" rel="noopener noreferrer">
-                <img src="img/linkedin-square-icon.png" alt="LinkedIn"/>
+                <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/linkedin-square-icon.png" alt="LinkedIn"/>
             </a>
         </div>
     </footer>    
