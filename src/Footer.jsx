3cc755a58b9b4ac5f601c8d7563c670c3f1a6f85
henna <henna.rajakisto@gmail.com>
2025-04-25 20:27:33 +0300

    linkedin icon fixed

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,9 +3,7 @@ export default function Footer() {
 return (
     <footer>
         <div>
-            <a href="https://www.linkedin.com/in/henna-rajakisto-b57b99253" target="_blank" rel="noopener noreferrer">
-            LinkedIn
-            </a>
+            <a href="https://www.linkedin.com/in/henna-rajakisto-b57b99253/" target="_blank" rel="noopener noreferrer"><img src="/img/linkedin-square-icon.png" alt="linkedin icon" className="icon" /></a>
         </div>
     </footer>    
     )}
