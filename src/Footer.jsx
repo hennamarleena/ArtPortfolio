da458728dc207e8d45248f6d543beba45ffae83a
henna <henna.rajakisto@gmail.com>
2026-05-19 13:04:32 +0300

    nimi lisätty footeriin

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,8 +2,6 @@ export default function Footer() {
     
 return (
     <footer>
-        <div>
-            <a href="https://www.linkedin.com/in/henna-rajakisto-b57b99253/" target="_blank" rel="noopener noreferrer"><img src="/img/linkedin-square-icon.png" alt="linkedin icon" className="icon" /></a>
-        </div>
+        <p>© 2026 Henna Rajakisto</p>
     </footer>    
     )}
